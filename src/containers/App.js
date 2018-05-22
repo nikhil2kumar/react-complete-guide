879fbb9dffb7c6568bb6cc2d720a5f2d0902a048
nikhil2kumar <kumarnikhil083@gmail.com>
2018-05-22 11:16:03 +0530

    Test2

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,14 +6,14 @@ import Cockpit from '../components/Cockpit/Cockpit';
 class App extends Component {
 
   constructor(props) {
-    super(props);   
+    super(props);
     this.state = {
       persons: [
         { id: '1', name: 'Nikhil1', age: 26 },
         { id: '2', name: 'Nikhil2', age: 27 }
       ],
       showPerson: false
-    } 
+    }
   }
   // state = {
   //   persons: [
@@ -22,7 +22,7 @@ class App extends Component {
   //   ],
   //   showPerson: false
   // }
-  
+
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => p.id === id);
     const person = { ...this.state.persons[personIndex] };
@@ -56,7 +56,7 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
-        <Cockpit showPerson={this.state.showPerson} clicked={this.togglePersonsHandler}/>
+        <Cockpit showPerson={this.state.showPerson} clicked={this.togglePersonsHandler} />
         {persons}
       </div>
     );
